47950c49427751664251ee928383c89a26293815
Ronbout <ronbout@yahoo.com>
2022-11-27 16:47:34 -0600

    Initial commit of shoponix dev environment

---
+++ next.config.js
@@ -0,0 +1,25 @@
+// must restart server whenever you make changes in next.config
+
+/** @type {import('next').NextConfig} */
+const path = require("path");
+
+const nextConfig = {
+  env: {
+    MONGO_SRV:
+      "mongodb+srv://moshoppa_db_user:VLYQPrpLvllCIX92@cluster0.h5ssnil.mongodb.net/?retryWrites=true&w=majority", // Update here your mongodb database URL
+    JWT_SECRET: "r++3RX7G+rGINHVWNnoSp+uCwa6E+KvdrWRb0FRgctw=", // Update here your JWT_SECRET
+    CLOUDINARY_URL:
+      "cloudinary://783796461164214:aC71NfvndxLkhZtIA_2Jk3r7MhI@dhrcafda6",
+    STRIPE_SECRET_KEY: "xxx", // Update here your STRIPE_SECRET_KEY
+  },
+
+  // reactStrictMode: true,
+  swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  trailingSlash: true,
+  optimizeFonts: false,
+  distDir: "build",
+};
+module.exports = nextConfig;
