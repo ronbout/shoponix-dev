b8c66c81a01b9975ce6e7fe242affb0f3e533bd4
Ronbout <ronbout@yahoo.com>
2022-11-28 08:48:10 -0600

    Fix the cloudinary image upload code to use the correct settings.  Also, moved cloudinary personal info to .env.local file

--- next.config.js
+++ next.config.js
@@ -8,8 +8,9 @@ const nextConfig = {
     MONGO_SRV:
       "mongodb+srv://moshoppa_db_user:VLYQPrpLvllCIX92@cluster0.h5ssnil.mongodb.net/?retryWrites=true&w=majority", // Update here your mongodb database URL
     JWT_SECRET: "r++3RX7G+rGINHVWNnoSp+uCwa6E+KvdrWRb0FRgctw=", // Update here your JWT_SECRET
-    CLOUDINARY_URL:
-      "cloudinary://783796461164214:aC71NfvndxLkhZtIA_2Jk3r7MhI@dhrcafda6",
+    //   // CLOUDINARY_URL:
+    //   //   "cloudinary://783796461164214:aC71NfvndxLkhZtIA_2Jk3r7MhI@dhrcafda6",
+    //   CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/dhrcafda6/image/upload",
     STRIPE_SECRET_KEY: "xxx", // Update here your STRIPE_SECRET_KEY
   },
 
