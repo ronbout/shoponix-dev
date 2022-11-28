75b4d7a06dc58ea6a473079ed70dc1b6a4f922ca
Ronbout <ronbout@yahoo.com>
2022-11-28 11:41:11 -0600

    updated import of uuid module for new version

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -1,5 +1,5 @@
 import Stripe from "stripe";
-import uuidv4 from "uuid/v4";
+import { v4 as uuidv4 } from "uuid";
 import jwt from "jsonwebtoken";
 import Cart from "../../models/Cart";
 import Order from "../../models/Order";
@@ -8,60 +8,57 @@ import calculateCartTotal from "../../utils/calculateCartTotal";
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async (req, res) => {
-	const { paymentData } = req.body;
+  const { paymentData } = req.body;
 
-	try {
-		const { userId } = jwt.verify(
-			req.headers.authorization,
-			process.env.JWT_SECRET
-		);
-		const cart = await Cart.findOne({ user: userId }).populate({
-			path: "products.product",
-			model: "Product",
-		});
-		const { cartTotal, stripeTotal } = calculateCartTotal(cart.products);
-		const prevCustomer = await stripe.customers.list({
-			email: paymentData.email,
-			limit: 1,
-		});
-		const isExistingCustomer = prevCustomer.data.length > 0;
-		let newCustomer;
-		if (!isExistingCustomer) {
-			newCustomer = await stripe.customers.create({
-				email: paymentData.email,
-				source: paymentData.id,
-			});
-		}
-		const customer =
-			(isExistingCustomer && prevCustomer.data[0].id) || newCustomer.id;
-		await stripe.charges.create(
-			{
-				currency: "usd",
-				amount: stripeTotal,
-				receipt_email: paymentData.email,
-				customer,
-				description: `Checkout | ${paymentData.email} | ${paymentData.id}`,
-			},
-			{
-				idempotency_key: uuidv4(),
-			}
-		);
+  try {
+    const { userId } = jwt.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET
+    );
+    const cart = await Cart.findOne({ user: userId }).populate({
+      path: "products.product",
+      model: "Product",
+    });
+    const { cartTotal, stripeTotal } = calculateCartTotal(cart.products);
+    const prevCustomer = await stripe.customers.list({
+      email: paymentData.email,
+      limit: 1,
+    });
+    const isExistingCustomer = prevCustomer.data.length > 0;
+    let newCustomer;
+    if (!isExistingCustomer) {
+      newCustomer = await stripe.customers.create({
+        email: paymentData.email,
+        source: paymentData.id,
+      });
+    }
+    const customer =
+      (isExistingCustomer && prevCustomer.data[0].id) || newCustomer.id;
+    await stripe.charges.create(
+      {
+        currency: "usd",
+        amount: stripeTotal,
+        receipt_email: paymentData.email,
+        customer,
+        description: `Checkout | ${paymentData.email} | ${paymentData.id}`,
+      },
+      {
+        idempotency_key: uuidv4(),
+      }
+    );
 
-		await new Order({
-			user: userId,
-			email: paymentData.email,
-			total: cartTotal,
-			products: cart.products,
-		}).save();
+    await new Order({
+      user: userId,
+      email: paymentData.email,
+      total: cartTotal,
+      products: cart.products,
+    }).save();
 
-		await Cart.findOneAndUpdate(
-			{ _id: cart._id },
-			{ $set: { products: [] } }
-		);
+    await Cart.findOneAndUpdate({ _id: cart._id }, { $set: { products: [] } });
 
-		res.status(200).send("Checkout successful!");
-	} catch (error) {
-		console.error(error);
-		res.status(500).send("Error proccessing charge");
-	}
+    res.status(200).send("Checkout successful!");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error proccessing charge");
+  }
 };
