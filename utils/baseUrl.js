cc27550cf0c124502f1dc04bb94d338eae11de40
Ronbout <ronbout@yahoo.com>
2022-11-28 19:40:55 -0600

    Debugging baseurl  4

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,7 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://shoponix.envytheme.com"
-    : "http://localhost:3000";
+    ? "https://shoponix-vercel.vercel.app/"
+    : // "https://shoponix.envytheme.com"
+      "http://localhost:3000";
 
 export default baseUrl;
