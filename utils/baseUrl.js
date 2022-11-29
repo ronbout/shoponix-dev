eb5ac89948dcc8eb59c41872a1a9a4c75e1fde0b
Ronbout <ronbout@yahoo.com>
2022-11-28 19:16:50 -0600

    change the baseUrl code to use the vercel_url preset environment variable

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,6 @@
-const baseUrl = process.env.NODE_ENV === "production" 
-? 'https://shoponix.envytheme.com' 
-: 'http://localhost:3000';
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? process.env.NEXT_PUBLIC_VERCEL_URL
+    : "http://localhost:3000";
 
 export default baseUrl;
