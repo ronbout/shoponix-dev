7bb1323340f6f0e4a0610270514a908b284a75c1
Ronbout <ronbout@yahoo.com>
2022-12-14 20:32:04 -0600

    Update the product get api for better filtering.  it can filter against the name, categories and tags as the default.  If a cat query is set, it will search only by Category.

--- pages/api/products.js
+++ pages/api/products.js
@@ -2,22 +2,59 @@ import prisma from "../../lib/prisma";
 
 export default async (req, res) => {
   try {
-    const { page = 1, size = 100, searchTerm = "" } = req.query;
+    const { page = 0, size = 0, searchTerm = "", cat = false } = req.query;
     // console.log(searchTerm)
     // string to number
     const pageNum = Number(page);
     const pageSize = Number(size);
-    const skips = pageSize * (pageNum - 1);
+    const skips = pageSize ? pageSize * (pageNum - 1) : 0;
     let products = [];
-    let totalDocts;
+    let totalProducts;
+
+    let whereClause = false;
     if (searchTerm) {
-      totalDocts = await prisma.product.count({
-        where: {
+      if (cat) {
+        whereClause = {
           OR: [
             {
-              productType: {
-                contains: searchTerm,
-                mode: "insensitive",
+              productCategories: {
+                some: {
+                  category: {
+                    name: {
+                      contains: searchTerm,
+                      mode: "insensitive",
+                    },
+                  },
+                },
+              },
+            },
+          ],
+        };
+      } else {
+        whereClause = {
+          OR: [
+            {
+              productCategories: {
+                some: {
+                  category: {
+                    name: {
+                      contains: searchTerm,
+                      mode: "insensitive",
+                    },
+                  },
+                },
+              },
+            },
+            {
+              productTags: {
+                some: {
+                  tag: {
+                    name: {
+                      contains: searchTerm,
+                      mode: "insensitive",
+                    },
+                  },
+                },
               },
             },
             {
@@ -27,54 +64,21 @@ export default async (req, res) => {
               },
             },
           ],
-        },
-      });
-      // totalDocts = await Product.countDocuments({
-      //   $or: [
-      //     { productType: { $regex: `^${searchTerm}` } },
-      //     { name: new RegExp(searchTerm, "i") },
-      //   ],
-      // });
-    } else {
-      totalDocts = await prisma.product.count();
-      // totalDocts = await Product.countDocuments();
+        };
+      }
     }
-    const totalPages = Math.ceil(totalDocts / pageSize);
+
+    if (searchTerm) {
+      totalProducts = await prisma.product.count({
+        ...(whereClause && { where: whereClause }),
+      });
+    } else {
+      totalProducts = await prisma.product.count();
+    }
+    const totalPages = pageSize ? Math.ceil(totalProducts / pageSize) : 1;
     if (searchTerm) {
       products = await prisma.product.findMany({
-        where: {
-          OR: [
-            {
-              productType: {
-                contains: searchTerm,
-                mode: "insensitive",
-              },
-            },
-            {
-              name: {
-                contains: searchTerm,
-                mode: "insensitive",
-              },
-            },
-          ],
-        },
-        orderBy: {
-          viewCount: "desc",
-        },
-        skip: skips,
-        take: pageSize,
-      });
-      // products = await Product.find({
-      //   $or: [
-      //     { productType: { $regex: `^${searchTerm}` } },
-      //     { name: new RegExp(searchTerm, "i") },
-      //   ],
-      // })
-      //   .collation({ locale: "en", strength: 1 })
-      //   .sort({ viewCount: "desc" })
-      //   .limit(pageSize);
-    } else {
-      products = await prisma.product.findMany({
+        ...(whereClause && { where: whereClause }),
         orderBy: {
           viewCount: "desc",
         },
@@ -90,23 +94,32 @@ export default async (req, res) => {
             },
           },
         },
-        skip: skips,
-        take: pageSize,
+        ...(pageSize && { skip: skips, take: pageSize }),
       });
-
-      // if (pageNum === 1) {
-      //   products = await Product.find().sort({ name: "desc" }).limit(pageSize);
-      // } else {
-      //   const skips = pageSize * (pageNum - 1);
-      //   products = await Product.find()
-      //     .sort({ name: "desc" })
-      //     .skip(skips)
-      //     .limit(pageSize);
-      // }
+    } else {
+      products = await prisma.product.findMany({
+        ...(whereClause && { where: whereClause }),
+        orderBy: {
+          viewCount: "desc",
+        },
+        include: {
+          productCategories: {
+            include: {
+              category: true,
+            },
+          },
+          productTags: {
+            include: {
+              tag: true,
+            },
+          },
+        },
+        ...(pageSize && { skip: skips, take: pageSize }),
+      });
     }
 
     // const products = await Product.find();
-    res.status(200).json({ products, totalPages });
+    res.status(200).json({ products, totalPages, totalProducts });
   } catch (error) {
     res.status(403).json(error);
   }
